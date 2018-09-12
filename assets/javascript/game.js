ad7c0d9dc5d2df4bef2b36086688184eea0ce954
jnadzieja <joshua.nadzieja@gmail.com>
2018-09-11 22:12:22 -0400

    final update to psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,28 @@
   var wins = 0;
   var losses = 0;
   var guesses = 9;
-  var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var letters = ["a", "b", "c", 'd', "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
   var winsText = document.getElementById("wins-text")
   var lossesText = document.getElementById("losses-text")
   var remainingGuesses = document.getElementById("remaining-guesses")
   var guessesText = document.getElementById("guesses-text")
+  var youLose = document.getElementById("you-lose")
 
-  document.onkeyup = function(event) {
+  document.onkeyup = function (event) {
 
     var userGuess = event.key.toLowerCase()
     var psychicLetter = letters[Math.floor(Math.random() * letters.length)]
-    
+
 
     if (userGuess === psychicLetter) {
       wins++;
-      guesses--;
     } else if (userGuess !== psychicLetter) {
-      losses++;
       guesses--;
+    } else if (guesses === 0) {
+      losses++;
+      youLose.textContent = "You lose!"
+      return;
     }
 
     guessesText.textContent = userGuess;
